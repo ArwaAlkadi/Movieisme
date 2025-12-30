b5a68019d79ba3f7d06469dbbf01a5612b733934
Latifa7373 <latifaalmawash73@gmail.com>
2025-12-30 17:04:18 +0300

    اضفت صوره الخلفيه في ال Assets
    وجهزت البيانات في ال ViewModel

--- MoviesApp/Pages/SignIn/SignInView.swift
+++ MoviesApp/Pages/SignIn/SignInView.swift
@@ -5,14 +5,3 @@
 //  Created by Arwa Alkadi on 24/12/2025.
 //
 
-import SwiftUI
-
-struct SignInView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    SignInView()
-}
--- MoviesApp/Pages/SignIn/SignInViewModel.swift
+++ MoviesApp/Pages/SignIn/SignInViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Combine
+
+class SignInViewModel: ObservableObject {
+    
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var isPasswordVisible: Bool = false
+    @Published var showError: Bool = false
+    
+    var isSignInButtonEnabled: Bool {
+        get {
+            return !self.email.isEmpty && !self.password.isEmpty
+        }
+    }
+    
+    func signIn() {
+        
+        print("Email: \(self.email)")
+        print("Password: \(self.password)")
+    }
+    
+    func togglePasswordVisibility() {
+        self.isPasswordVisible.toggle()
+    }
+}
